ac570e75e65a5102d5c96c1966217beebee911ec
Stanislav <stasperfiliyev@gmail.com>
2023-06-06 23:38:58 +0600

    sample_1

---
+++ Lesson_1/sample1.java
@@ -0,0 +1,26 @@
+package Lesson_1;
+
+/**
+ * sample1
+ */
+
+ import java.util.Scanner;
+
+public class sample1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, "cp866");
+        System.out.print("Enter name: ");
+        String name = sc.next();
+
+        // .nextLine()   --- Считывает строчку 
+        // .next() --- считывает элемент
+        // .nextInt() --- считывает Число
+
+        //12 \n 
+        // num = 12
+        // .... sc.nextLine(); //
+        System.out.printf("HI, %s", name);
+        sc.close();
+
+    }
+}        
---
+++ Lesson_1/sample2.java
@@ -0,0 +1,26 @@
+package Lesson_1;
+
+/**
+ * sample1
+ */
+
+ import java.util.Scanner;
+
+public class sample2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, "cp866");
+        System.out.print("Введите имя: ");
+        String name = sc.next();
+
+        // .nextLine()   --- Считывает строчку 
+        // .next() --- считывает элемент
+        // .nextInt() --- считывает Число
+
+        //12 \n 
+        // num = 12
+        // .... sc.nextLine(); //
+        System.out.printf("Привет, %s", name);
+        sc.close();
+
+    }
+}        

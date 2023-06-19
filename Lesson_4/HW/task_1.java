632b0b9c5aedb74c60be57425802eb439657b35e
Stanislav <stasperfiliyev@gmail.com>
2023-06-19 09:41:07 +0600

    HW

--- Lesson_4/HW/task_1.java
+++ Lesson_4/HW/task_1.java
@@ -3,33 +3,55 @@ package Lesson_4.HW;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.LinkedList;
 
 //Даны два Deque представляющие два неотрицательных целых числа. Цифры хранятся в обратном порядке,
 // и каждый из их узлов содержит одну цифру.
 // Сложите два числа и верните сумму в виде связанного списка.
+// Умножьте два числа и верните произведение в виде связанного списка.
 
 public class task_1 {
     public static void main(String[] args) {
-        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(4, 2, -12, 4));
-        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(-1, 3, -6, 10));
-        task_1 task = new task_1();
-        Deque<Integer> result = task.sum(d1, d2);
-        System.out.println(result);
+
+        Deque <String> deq1 = new ArrayDeque<>();
+        Deque <String> deq2 = new ArrayDeque<>();
+        deq1.add("3");
+        deq1.add("2");
+        deq1.add("1");
+        deq1.add("-");
+        deq2.add("6");
+        deq2.add("5");
+        deq2.add("4");
+        deq2.add("-");
+        StringBuilder sb1 = new StringBuilder();
+        for (int i = deq1.size()-1; i >=0; i--) {
+            sb1.append(deq1.pollLast());
+        }
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = deq2.size()-1; i >=0; i--) {
+            sb2.append(deq2.pollLast());
+        }
+        String num1 = new StringBuilder(sb1).toString();
+        String num2 = new StringBuilder(sb2).toString();
+        int number1 = Integer.parseInt(num1);
+        int number2 = Integer.parseInt(num2);
+        int prod = number1*number2;
+        int sum = number1+number2;
+        System.out.println(sum);
+        String prodStr = ""+prod;
+        String sumStr = ""+sum;
+        char[] chars = prodStr.toCharArray();
+        LinkedList<Character> prodStrList = new LinkedList<>();
+        for (int i = 0; i < chars.length; i++) {
+            prodStrList.add(chars[i]);
+        }
+        System.out.println(prodStrList);
+        char[] chars2 = sumStr.toCharArray();
+        LinkedList<Character> sumStrList = new LinkedList<>();
+        for (int i = 0; i < chars2.length; i++) {
+            sumStrList.add(chars2[i]);
+        }
+        System.out.println(sumStrList);
     }
 
-    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
-        Deque<Integer> result = new ArrayDeque<>();
-        int carry = 0;
-        while (!d1.isEmpty() || !d2.isEmpty()) {
-            int sum = carry;
-            if (!d1.isEmpty()) {
-                sum += d1.pop();
-            }
-            if (!d2.isEmpty()) {
-                sum += d2.pop();
-            }
-            result.addLast(sum);
-        }
-        return result;
-    }
 }

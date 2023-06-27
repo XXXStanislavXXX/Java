684cd6919cb8427213f964bef685b0f6419c1877
Stanislav <stasperfiliyev@gmail.com>
2023-06-28 02:01:41 +0600

    ООП урок 1

---
+++ ООП/Lesson_1/Owner.java
@@ -0,0 +1,14 @@
+package ООП.Lesson_1;
+
+public class Owner {
+    private String name;
+    public Owner(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return name;
+    }
+}
+

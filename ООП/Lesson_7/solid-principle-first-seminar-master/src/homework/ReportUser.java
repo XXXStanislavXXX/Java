1b6570363caa5e87511eb8c633b99238d49f80d1
Stanislav <stasperfiliyev@gmail.com>
2023-07-17 23:28:25 +0600

    Hw

---
+++ ООП/Lesson_7/solid-principle-first-seminar-master/src/homework/ReportUser.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class ReportUser implements Reportable{
+
+    private User user;
+
+    public ReportUser(User user){
+        this.user = user;
+    }
+
+    public void report(){
+        System.out.println("Report for user: " + user.getName());
+    }
+
+}
---
+++ ООП/Lesson_7/solid-principle-first-seminar-master/src/homework/Saveable.java
@@ -0,0 +1,7 @@
+package homework;
+
+public interface Saveable {
+
+    public void save();
+
+}
